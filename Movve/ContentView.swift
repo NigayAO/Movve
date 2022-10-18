52accf9569888eb4fa45e7f60fe2657cc371f640
Alik <Nigay_AO@bk.ru>
2022-10-18 21:03:10 +0600

    Initial Commit

---
+++ Movve/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Movve
+//
+//  Created by Alik Nigay on 18.10.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Movve/MovveApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovveApp.swift
+//  Movve
+//
+//  Created by Alik Nigay on 18.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct MovveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
